670456d688ce0f1f7751fdf788e3f5328468b5d2
Edward Betts <edward@4angle.com>
2017-03-06 11:37:21 +0000

    filter larger result counts

--- update_total_hits.py
+++ update_total_hits.py
@@ -12,18 +12,22 @@ for t in Term.query:
     results = wikipedia.wiki_search(t.get_query())
     t.total_hits = results.total_hits
 
-    if results.total_hits < 50:
-        safe_articles = {doc.title for doc in t.safe_articles}
-        pattern = '|'.join(re.escape(safe.phrase) for safe in t.safe_phrases)
-        re_phrase = re.compile('(' + pattern + ')', re.I)
+    if results.total_hits < 500:
+        while True:
+            safe_articles = {doc.title for doc in t.safe_articles}
+            pattern = '|'.join(re.escape(safe.phrase) for safe in t.safe_phrases)
+            re_phrase = re.compile('(' + pattern + ')', re.I)
 
-        for doc in results.docs:
-            if doc['title'] in safe_articles:
-                t.total_hits -= 1
-            if t.term.lower() not in doc['text'].lower():
-                t.total_hits -= 1
-            if pattern and re_phrase.search(doc['text']):
-                t.total_hits -= 1
+            for doc in results.docs:
+                if doc['title'] in safe_articles:
+                    t.total_hits -= 1
+                if t.term.lower() not in doc['text'].lower():
+                    t.total_hits -= 1
+                if pattern and re_phrase.search(doc['text']):
+                    t.total_hits -= 1
+            if not results.next_offset:
+                break
+            results = wikipedia.wiki_search(t.get_query(), offset=results.next_offset)
 
     print('{:14s} {:5} -> {:5d}'.format(t.term, old or '', t.total_hits))
     session.add(t)
